07c8a113736de5520afd48fa37cd66456ccbc050
JeremiasDief <jeremias_dief@yahoo.com.br>
2023-05-20 18:22:36 -0300

    Mudando o texto da url para 'football'

--- Next_Matches.py
+++ Next_Matches.py
@@ -75,13 +75,13 @@ options.add_argument('--remote-debugging-port=9222')
 #options.binary_location = '/usr/bin/google-chrome'
 #options.add_argument('window-size=400,800')
 
-hoje = date.today()#-timedelta(1)
+hoje = date.today()-timedelta(1)
 hoje_ano = hoje.year
 hoje_mes = hoje.month
 hoje_dia = hoje.day
 
 url_base = "https://www.betexplorer.com"
-url_nextmatches = f'https://www.betexplorer.com/next/soccer/?year={hoje_ano}&month={hoje_mes}&day={hoje_dia}'
+url_nextmatches = f'https://www.betexplorer.com/next/football/?year={hoje_ano}&month={hoje_mes}&day={hoje_dia}'
 
 #navegador = webdriver.Chrome(options=options, service=service, executable_path="/usr/local/bin/chromedriver")
 navegador = webdriver.Chrome(options=options, service=service)
