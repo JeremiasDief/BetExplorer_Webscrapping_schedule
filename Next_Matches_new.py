71e0f771035f079366b72a6072a812463b43eaa6
JeremiasDief <jeremias_dief@yahoo.com.br>
2024-05-24 18:09:45 -0300

    Adicionado mais depuradores

--- Next_Matches_new.py
+++ Next_Matches_new.py
@@ -10,6 +10,7 @@ import time
 from datetime import date, timedelta
 import pandas as pd
 import numpy as np
+import sys
 
 # Credenciais
 login = "jeremias_dief"
@@ -110,6 +111,9 @@ try:
     next_matches_table = site.find('div', attrs={'id': 'nr-ko-all'})
     if next_matches_table:
         print("next_matches_table ok!")
+        # Salvar a variável em um arquivo txt
+        with open('"./Next_Matches/next_matches_table.txt', 'w', encoding='utf-8') as file:
+            file.write(str(next_matches_table))
     else:
         print("next_matches_table não puxou")
 
@@ -119,6 +123,7 @@ try:
         print("leagues ok!")
     else:
         print("leagues não puxou")
+        sys.exit("Erro: leagues não foi encontrado.")
     for league in leagues:
         favourite_star = league.find('a', class_='myleague active table-main__FavouriteStar')
         if favourite_star:
